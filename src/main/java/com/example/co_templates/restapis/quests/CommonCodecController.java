9e006e0ad38eb85feac86511301124f30e6fa6d9
rugkfl <kimgyoungha61@gmail.com>
2024-04-09 09:20:34 +0000

    first commit

---
+++ src/main/java/com/example/co_templates/restapis/quests/CommonCodecController.java
@@ -0,0 +1,42 @@
+package com.example.co_templates.restapis.quests;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class CommonCodecController {
+    @GetMapping("/commonCode/list/{pk_id}") 
+    public ResponseEntity<Object> commonCodeList(@PathVariable String pk_id) {
+    
+        ArrayList arrayList = new ArrayList<>();
+        HashMap resultMap = new HashMap<>();
+
+        resultMap.put("pk_id", 1) ;
+        resultMap.put("fk_id", 1234) ;
+        resultMap.put("name", "gyungha") ;
+        arrayList.add(resultMap) ;
+
+        return ResponseEntity.ok().body(arrayList);
+  
+    }
+
+    @GetMapping("/commonCode/view/{pk_id}/{fk_id}/{name}")
+    public ResponseEntity<Object> commonCodeHashMap (@PathVariable String pk_id, @PathVariable String fk_id, @PathVariable String name) {
+        ArrayList arrayList = new ArrayList<>();
+        HashMap resultMap = new HashMap<>();
+
+        resultMap.put("pk_id", "pk_01");
+        resultMap.put("fk_id", "fk_01");
+        resultMap.put("name", "gyungha");
+
+        return ResponseEntity.ok().body(resultMap);
+    }
+    
+}
